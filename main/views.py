02288ef5fd82d1ac2b2b4ac24f63af26b792e721
Zak Timson <ztimson@auxiliumgroup.com>
2017-08-18 23:37:16 -0400

    Added logout

--- main/views.py
+++ main/views.py
@@ -1,4 +1,5 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import auth
 
 from charter_members.models import Attorney
 
@@ -6,3 +7,8 @@ from charter_members.models import Attorney
 def index(request):
     attorneys = Attorney.objects.filter(front_page=True)
     return render(request, 'index.html', {'attorneys': attorneys})
+
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
