bc6f2e1286edea48074766116a36fa35905f28a4
Zak Timson <ztimson@auxiliumgroup.com>
2017-09-04 13:32:21 -0400

    Revert "Added HTML editor"
    This reverts commit a8a4c8d

--- OACPL/urls.py
+++ OACPL/urls.py
@@ -1,7 +1,7 @@
 import os
 
 from django.conf import settings
-from django.conf.urls import include, url
+from django.conf.urls import url
 from django.contrib import admin
 from django.contrib.auth.decorators import login_required
 from django.views.static import serve
@@ -21,9 +21,8 @@ urlpatterns = [
     url(r'^admin/logout', main.views.logout, name='logout'),
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^attorney/(?P<id>\d+)', charter_members.views.index, name='attorney'),
-    url(r'^froala_editor/', include('froala_editor.urls')),
     url(r'^media/secure/(?P<path>.*)$', protected_serve, {'document_root': os.path.join(settings.MEDIA_ROOT, 'secure')}, name='secure media'),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}, name='media'),
     url(r'^newsletter/subscribe', newsletters.views.subscribe, name='subscribe'),
-    url(r'^newsletter/', newsletters.views.newsletters, name='newsletters'),
+    url(r'^newsletter/', newsletters.views.newsletters, name='newsletters')
 ]
