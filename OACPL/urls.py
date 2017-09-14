df148909bf2eee492f2dcf03804b2a988e77837c
Zak Timson <ztimson@auxiliumgroup.com>
2017-09-14 13:52:39 -0400

    Created expert witnesses pages

--- OACPL/urls.py
+++ OACPL/urls.py
@@ -10,6 +10,7 @@ from django.views.static import serve
 import main.views
 import case_law.views
 import charter_members.views
+import expert_witnesses.views
 import newsletters.views
 
 
@@ -25,7 +26,9 @@ urlpatterns = [
     url(r'^admin/logout', main.views.logout, name='logout'),
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^attorney/(?P<id>\d+)', charter_members.views.index, name='attorney'),
-    url(r'^caselaw/', case_law.views.browser, name='browser'),
+    url(r'^caselaw/', case_law.views.browser, name='caselaw'),
+    url(r'^experts/', expert_witnesses.views.browser, name='experts'),
+    url(r'^experts/(<?P<id>\d+)', expert_witnesses.views.viewer, name='expert'),
     url(r'^media/secure/(?P<path>.*)$', protected_serve, {'document_root': os.path.join(settings.MEDIA_ROOT, 'secure')}, name='secure media'),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}, name='media'),
     url(r'^newsletter/subscribe', newsletters.views.subscribe, name='subscribe'),
--- expert_witnesses/views.py
+++ expert_witnesses/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
-# Create your views here.
+from .models import AreaOfExpertise, Expert
+
+
+def browser(request):
+    filters = AreaOfExpertise.objects.all().order_by('field')
+    filter = filters.filter(field=request.POST.get('filter')).values_list('field')
+    experts = Expert.objects.all()
+    if filter:
+        experts = experts.filter(expertise__in=filter)
+
+    return render(request, 'expertBrowser.html', {'experts': experts, 'filters': filters})
+
+
+def viewer(request, id):
+    expert = Expert.objects.get(id=id)
+    return render(request, 'expertViewer.html', {'expert': expert})
