243043eb5ab97b37192c7eac0281f0aecdcc1ed1
Zak Timson <ztimson@auxiliumgroup.com>
2017-09-15 10:48:13 -0400

    Updated static files settings

--- OACPL/settings.base.py
+++ OACPL/settings.base.py
@@ -129,9 +129,7 @@ USE_TZ = True
 
 BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
