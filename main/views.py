ef69d576612c6291efa486776aacbf9e7860f90f
Zak Timson <ztimson@auxiliumgroup.com>
2017-09-15 12:41:20 -0400

    Added backend for login page

--- main/views.py
+++ main/views.py
@@ -1,9 +1,9 @@
-import json
-
-from django.shortcuts import HttpResponse, render, redirect
+from django.shortcuts import render, redirect
 from django.contrib import auth
+from django.contrib.auth.models import User
 
 from charter_members.models import Attorney
+from newsletters.models import Subscriber
 
 
 def index(request):
@@ -13,13 +13,30 @@ def index(request):
 
 def login(request):
     if request.method == 'POST':
-        user = auth.authenticate(request)
-        if user:
-            return HttpResponse(json.dumps({'status': True}))
-        else:
-            return HttpResponse(json.dumps({'status': False}))
+        if request.POST.get('request') == 'login':
+            user = auth.authenticate(request, username=request.POST.get('username'), password=request.POST.get('password'))
+            if user:
+                auth.login(request, user)
+                return redirect('/')
+            else:
+                return render(request, 'login.html', {'navbar': False, 'footer': False, 'failed': True})
+        elif request.POST.get('request') == 'register':
+            user = User.objects.create_user(request.POST.get('username'),
+                                            email=request.POST.get('email'),
+                                            password=request.POST.get('password'))
+            user.save()
+            # TODO: Send confirmation email
+            if request.POST.get('newsletter'):
+                Subscriber.objects.create(email=request.POST.get('email'))
+            # TODO: If Case law access was requested, send an email out to staff
+            auth.login(request, user)
+            return redirect('/')
+
+        elif request.POST.get('request') == 'reset':
+            # TODO: Reset password and send email
+            pass
     else:
-        return render(request, 'login.html')
+        return render(request, 'login.html', {'navbar': False, 'footer': False})
 
 
 def logout(request):
