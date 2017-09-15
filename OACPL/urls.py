7c2fd01b3f0655a9c7eb06cde793c47f5521a1a1
Zak Timson <ztimson@auxiliumgroup.com>
2017-09-15 10:50:24 -0400

    Created basic login page

--- OACPL/urls.py
+++ OACPL/urls.py
@@ -23,12 +23,14 @@ def protected_serve(request, path, document_root=None, show_indexes=False):
 
 urlpatterns = [
     url(r'^$', main.views.index, name='home'),
-    url(r'^admin/logout', main.views.logout, name='logout'),
+    url(r'^admin/logout', main.views.logout),
     url(r'^admin/', admin.site.urls, name='admin'),
     url(r'^attorney/(?P<id>\d+)', charter_members.views.index, name='attorney'),
     url(r'^caselaw/', case_law.views.browser, name='caselaw'),
     url(r'^experts/', expert_witnesses.views.browser, name='experts'),
     url(r'^experts/(<?P<id>\d+)', expert_witnesses.views.viewer, name='expert'),
+    url(r'^login/', main.views.login, name='login'),
+    url(r'^logout/', main.views.logout, name='logout'),
     url(r'^media/secure/(?P<path>.*)$', protected_serve, {'document_root': os.path.join(settings.MEDIA_ROOT, 'secure')}, name='secure media'),
     url(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}, name='media'),
     url(r'^newsletter/subscribe', newsletters.views.subscribe, name='subscribe'),
--- main/views.py
+++ main/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect
+import json
+
+from django.shortcuts import HttpResponse, render, redirect
 from django.contrib import auth
 
 from charter_members.models import Attorney
@@ -7,6 +9,17 @@ from charter_members.models import Attorney
 def index(request):
     attorneys = Attorney.objects.filter(front_page=True)
     return render(request, 'index.html', {'attorneys': attorneys})
+
+
+def login(request):
+    if request.method == 'POST':
+        user = auth.authenticate(request)
+        if user:
+            return HttpResponse(json.dumps({'status': True}))
+        else:
+            return HttpResponse(json.dumps({'status': False}))
+    else:
+        return render(request, 'login.html')
 
 
 def logout(request):
