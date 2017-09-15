65aa2c4ee8ba49d1c1a5154b788578fff5ba8d6e
Zak Timson <ztimson@auxiliumgroup.com>
2017-09-15 10:48:42 -0400

    Show newsletters that are published

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,12 +1,14 @@
 import json
 
+from django.db.models import Q
 from django.shortcuts import render, HttpResponse
+from django.utils import timezone
 
 from .models import Newsletter, Subscriber
 
 
 def newsletters(request):
-    newsletters = Newsletter.objects.filter(sent=True).order_by('-publish')
+    newsletters = Newsletter.objects.filter(Q(publish__lte=timezone.now())).order_by('-publish')
     return render(request, 'newsletters.html', {'newsletters': newsletters})
 
 
