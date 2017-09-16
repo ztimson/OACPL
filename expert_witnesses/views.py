ff2eb574e18bffce83c28cb2d5d9807c754cc040
Zak Timson <ztimson@auxiliumgroup.com>
2017-09-16 18:45:28 -0400

    Added filters

--- expert_witnesses/views.py
+++ expert_witnesses/views.py
@@ -5,12 +5,23 @@ from .models import AreaOfExpertise, Expert
 
 def browser(request):
     filters = AreaOfExpertise.objects.all().order_by('field')
-    filter = filters.filter(field=request.POST.get('filter')).values_list('field')
-    experts = Expert.objects.all()
-    if filter:
-        experts = experts.filter(expertise__in=filter)
 
-    return render(request, 'expertBrowser.html', {'experts': experts, 'filters': filters})
+    experts = Expert.objects.all()
+
+    institutes = set()
+    for expert in experts:
+        institutes.add(expert.institute) if expert.institute is not None else None
+
+    if request.GET.get('name'):
+        experts = experts.filter(name__contains=request.GET.get('name'))
+
+    if request.GET.get('institute'):
+        experts = experts.filter(institute=request.GET.get('institute'))
+
+    if request.GET.get('experties'):
+        experts = experts.filter(expertise=AreaOfExpertise.objects.filter(field=request.GET.get('experties'))).distinct()
+
+    return render(request, 'expertBrowser.html', {'experts': experts, 'institutes': institutes, 'filters': filters})
 
 
 def viewer(request, id):
