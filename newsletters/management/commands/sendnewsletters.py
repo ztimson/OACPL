0885c9ae4f16838a79c5e5e4c0d104101095ce7a
Zak Timson <zaktimson@gmail.com>
2017-09-29 19:02:04 -0400

    Added newsletter email template rendering

--- newsletters/management/commands/sendnewsletters.py
+++ newsletters/management/commands/sendnewsletters.py
@@ -3,6 +3,7 @@ from django.core import mail
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 from django.utils.html import strip_tags
+from django.template.loader import render_to_string
 
 from OACPL import settings
 from newsletters.models import Newsletter, Subscriber
@@ -18,7 +19,7 @@ class Command(BaseCommand):
         print('Found %s subscribers' % len(subscribers))
         for newsletter in newsletters:
             print('Sending newsletter: "$s"' % newsletter.subject)
-            mail.send_mail(newsletter.subject, strip_tags(newsletter.body), settings.EMAIL_HOST_USER, subscribers, html_message=newsletter.body)
+            mail.send_mail(newsletter.subject, strip_tags(newsletter.body), settings.EMAIL_HOST_USER, subscribers, html_message=render_to_string('email.html', {'content', newsletter.body}))
             newsletter.sent = True
             newsletter.save()
         print('Complete!')
