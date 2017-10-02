b0cbc63db53e8dfb37bab822399ec52391e74400
Zak Timson <zaktimson@gmail.com>
2017-10-02 15:41:05 -0400

    Filters save state in expert browser

--- expert_witnesses/views.py
+++ expert_witnesses/views.py
@@ -4,7 +4,7 @@ from .models import AreaOfExpertise, Expert
 
 
 def browser(request):
-    filters = AreaOfExpertise.objects.all().order_by('field')
+    aoe = AreaOfExpertise.objects.all().order_by('field')
 
     experts = Expert.objects.all()
 
@@ -12,16 +12,20 @@ def browser(request):
     for expert in experts:
         institutes.add(expert.institute) if expert.institute is not None else None
 
-    if request.GET.get('name'):
+    name = request.GET.get('name')
+    institute = request.GET.get('institute')
+    expertise = request.GET.get('expertise')
+
+    if name:
         experts = experts.filter(name__contains=request.GET.get('name'))
 
-    if request.GET.get('institute'):
+    if institute:
         experts = experts.filter(institute=request.GET.get('institute'))
 
-    if request.GET.get('experties'):
-        experts = experts.filter(expertise=AreaOfExpertise.objects.filter(field=request.GET.get('experties'))).distinct()
+    if expertise:
+        experts = experts.filter(expertise=AreaOfExpertise.objects.filter(field=expertise)).distinct()
 
-    return render(request, 'expertBrowser.html', {'experts': experts, 'institutes': institutes, 'filters': filters})
+    return render(request, 'expertBrowser.html', {'experts': experts, 'institutes': institutes, 'aoe': aoe, 'name': name, 'institute': institute, 'expertise': expertise})
 
 
 def viewer(request, id):
