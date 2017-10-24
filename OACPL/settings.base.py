167939beaef2246f25f59b14090e8d9d8e17d304
Zak Timson <zaktimson@gmail.com>
2017-10-23 23:14:21 -0400

    Added sandor as email contact

--- OACPL/settings.base.py
+++ OACPL/settings.base.py
@@ -34,7 +34,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ontarioacpl@gmail.com'
 EMAIL_HOST_PASSWORD = 'b5q?E8bhE7br*~)Y'
 EMAIL_PORT = 587
-EMAIL_CONTACT = 'zaktimson@gmail.com'
+EMAIL_CONTACT = 'david.a.sandor@outlook.com'
 
 # Application definition
 
