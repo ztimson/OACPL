667a470f4153b1bb5d73b76c846a6f7c8b71e212
Zak Timson <zaktimson@gmail.com>
2018-01-14 16:48:55 -0500

    Updated newsletter subscription system

--- newsletters/views.py
+++ newsletters/views.py
@@ -1,20 +1,14 @@
 import json
 
 from django.db.models import Q
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render
 from django.utils import timezone
 
 from .models import Newsletter, Subscriber
 
 
 def newsletters(request):
+    if request.method == 'POST':
+        Subscriber.objects.create(email=request.POST.get('email'))
     newsletters = Newsletter.objects.filter(Q(publish__lte=timezone.now())).order_by('-publish')
     return render(request, 'newsletters.html', {'newsletters': newsletters})
-
-
-def subscribe(request):
-    try:
-        Subscriber.objects.create(email=request.POST.get('email'))
-        return HttpResponse(json.dumps({'success': True}), content_type='json')
-    except Exception:
-        return HttpResponse(json.dumps({'success': False}), content_type='json')
