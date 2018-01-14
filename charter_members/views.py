1c1b56722ca3e74e4dcaffc54f9cd6ec34e191fc
Zak Timson <zaktimson@gmail.com>
2018-01-14 17:55:35 -0500

    Created a members page

--- charter_members/views.py
+++ charter_members/views.py
@@ -1,8 +1,15 @@
+from django.db.models import Count
 from django.shortcuts import render
 
-from .models import Attorney
+from .models import Chapter, Attorney
 
 
 def index(request, id):
     attorney = Attorney.objects.get(id=id)
     return render(request, 'attorney.html', {'attorney': attorney})
+
+
+def all(request):
+    chapters = Chapter.objects.all().order_by('name')
+    attorneys = Attorney.objects.all().annotate(Count('chapter'))
+    return render(request, 'all.html', {'chapters': chapters, 'attorneys': attorneys})
