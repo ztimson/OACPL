2cce096748b6951e758131ab1106c6e75f12e1ef
Zak Timson <zaktimson@gmail.com>
2018-01-14 21:17:40 -0500

    Added order column to admin grid

--- charter_members/admin.py
+++ charter_members/admin.py
@@ -9,7 +9,7 @@ admin.site.register(Position)
 
 @admin.register(Attorney)
 class MemberAdmin(admin.ModelAdmin):
-    list_display = ['name', 'position', 'chapter', 'phone_formatted', 'email', 'front_page', 'joined', 'thumbnail']
+    list_display = ['name', 'position', 'chapter', 'phone_formatted', 'email', 'front_page', 'order', 'joined', 'thumbnail']
     list_filter = ['chapter', 'position', 'front_page', 'joined']
     search_fields = ['email', 'joined', 'name', 'chapter', 'position', 'website', 'phone', 'phone_formatted']
     fields = ['image_preview', 'image', 'name', 'position', 'chapter', 'biography', 'phone', 'email', 'website', 'front_page', 'order', 'joined']
